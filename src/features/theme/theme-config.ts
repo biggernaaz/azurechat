ad3fe46a6f9be6dcae68b9e274a6a2bffc655ba1
biggernaaz <154835342+biggernaaz@users.noreply.github.com>
2024-07-09 02:51:14 -0400

    Update theme-config.ts
    Changed Azure to Aarohaa

--- src/features/theme/theme-config.ts
+++ src/features/theme/theme-config.ts
@@ -1,5 +1,5 @@
-export const AI_NAME = "Azure Chat";
-export const AI_DESCRIPTION = "Azure Chat is a friendly AI assistant.";
+export const AI_NAME = "AAROHAA Chat";
+export const AI_DESCRIPTION = "AAROHAA Chat is a friendly AI assistant built by Friends of AAROHAA.";
 export const CHAT_DEFAULT_PERSONA = AI_NAME + " default";
 
 export const CHAT_DEFAULT_SYSTEM_PROMPT = `You are a friendly ${AI_NAME} AI assistant. You must always return in markdown format.
